f301b00a7e2140ac3a63471287c70718c51a445a
kakacordovil <kakacordovil@gmail.com>
2020-05-11 00:47:55 +0100

    first commit

---
+++ Hello.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+puclic class Hello {
+  public static void main (String [] args) {
+	  String name;
+	  Scanner keyboard = new Scanner(System.in);
+
+	  System.out.println("Insert your name: ");
+	  name = keyboard.nextLine();
+
+	  
+	  System.out.println("Hello " + name);
+	  }
+}
